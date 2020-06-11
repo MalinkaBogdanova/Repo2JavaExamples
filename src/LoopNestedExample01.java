34dbafecd3b1a8897e4c57ebcc25451d325dc427
MalinkaBogdanova <malinka.bogdanova@gmail.com>
2020-06-11 12:43:51 +0300

    11/6/2020

---
+++ src/LoopNestedExample01.java
@@ -0,0 +1,21 @@
+public class LoopNestedExample01 {
+    /* output
+1	2	3	4
+5	6	7
+8	9
+10
+     */
+    public static void main(String[] args) {
+        int k =1;
+        for (int i = 0; i<=4; i++) {
+            System.out.println();
+            for (int j = 0; j <4-i; j++) {
+                System.out.print(k);
+                System.out.print("\t");
+                k++;
+
+            }
+
+        }
+    }
+}
